7945c1002eee02b230bac5332a91ed75a752f0e8
Finn <ftekverk@gmail.com>
2022-11-29 20:50:10 -0500

    new project template

---
+++ Project/app/src/main/java/com/example/ee193take2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ee193take2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.ee193take2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        Button testButton = findViewById(R.id.testButton);
+        TextView testText = findViewById(R.id.testText);
+
+
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                navController.navigate(R.id.blankFragment);
+
+                testText.setText("hi");
+            }
+        });
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/ssas/Attendance.java
+++
@@ -1,9 +0,0 @@
-package com.example.ssas;
-
-import java.util.Date;
-import java.util.List;
-
-public class Attendance {
-    private boolean presence;
-    private List<Date> dates;
-}
