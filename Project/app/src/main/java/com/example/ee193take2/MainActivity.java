27c6bfa8650780d2c2354c9e580629acc98fc8e0
Savannah-sun <yiqian.sun@tufts.edu>
2022-12-03 04:54:46 -0500

    create register & login UI and also some transaction

--- Project/app/src/main/java/com/example/ee193take2/MainActivity.java
+++ Project/app/src/main/java/com/example/ee193take2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.ee193take2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,7 +18,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.ee193take2.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView register;
+    private EditText username,password;
+    private Button login;
 
     private ActivityMainBinding binding;
 
@@ -24,40 +29,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.fragment_login);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
 
-        NavHostFragment navHostFragment =
-                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
-        NavController navController = navHostFragment.getNavController();
-
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
-
-        Button testButton = findViewById(R.id.testButton);
-        TextView testText = findViewById(R.id.testText);
-
-
-
-        testButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-                navController.navigate(R.id.blankFragment);
-
-                testText.setText("hi");
-            }
-        });
-
-
-
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
     }
+     @Override
+    public void onClick(View v){
+
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+            case R.id.login:
+                break;
+        }
+     }
+
+
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+////        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//
+//        NavHostFragment navHostFragment =
+//                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+//        NavController navController = navHostFragment.getNavController();
+//
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+//
+//        Button testButton = findViewById(R.id.testButton);
+//        TextView testText = findViewById(R.id.testText);
+
+
+
+//        testButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Do something in response to button click
+//                navController.navigate(R.id.blankFragment);
+//
+//                testText.setText("hi");
+//            }
+//        });
+//    }
 
 }
---
+++ Project/app/src/main/java/com/example/ee193take2/RegisterUser.java
@@ -0,0 +1,78 @@
+package com.example.ee193take2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+    private TextView banner;
+    private EditText name, school, email, password;
+    private Button register;
+
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        name = (EditText) findViewById(R.id.RegisterUsername);
+        school = (EditText) findViewById(R.id.School);
+        email = (EditText) findViewById(R.id.RegisterEmail);
+        password = (EditText) findViewById(R.id.textPassword);
+
+        register = (Button) findViewById(R.id.Register);
+        banner = (TextView) findViewById(R.id.banner);
+
+        banner.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.Register:
+                registerUser();
+                break;
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+
+        }
+    }
+
+    private void registerUser() {
+        String aemail = email.getText().toString().trim();
+        String aname = name.getText().toString().trim();
+        String apassword = password.getText().toString().trim();
+        String aschool = school.toString().toString().trim();
+        if(aname.isEmpty()){
+            name.setError("Name is required!");
+            name.requestFocus();
+            return;
+        }
+        if(aschool.isEmpty()){
+            school.setError("School is required!");
+            school.requestFocus();
+            return;
+        }
+        if(aemail.isEmpty()){
+            email.setError("Email is required!");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(aemail).matches()){
+            email.setError("Please provide a correct email address");
+            email.requestFocus();
+            return;
+        }
+
+        user = new User(aemail, apassword);
+    }
+}
