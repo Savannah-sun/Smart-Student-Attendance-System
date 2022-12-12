06bba64fd260e67c49bd8fb816dab83e58f581b4
msteinborn1 <mmullo01@tufts.edu>
2022-12-12 08:13:50 -0500

    added course offering display

---
+++ Project/app/src/main/java/com/example/ee193take2/ui/course_offering/CourseOfferingViewHolder.java
@@ -0,0 +1,47 @@
+package com.example.ee193take2.ui.course_offering;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ee193take2.Class_Offerings_Activity;
+import com.example.ee193take2.R;
+
+class CourseOfferingViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private final TextView courseItemView;
+
+    private CourseOfferingViewHolder(View itemView){
+        super(itemView);
+        courseItemView = itemView.findViewById(R.id.textView);
+        itemView.setOnClickListener(this);
+    }
+
+    public void bind(String text) {
+        courseItemView.setText(text);
+    }
+
+    static CourseOfferingViewHolder create(ViewGroup parent){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recylclerview_item,parent,false);
+
+        return new CourseOfferingViewHolder(view);
+    }
+
+
+    @Override
+    public void onClick(View v){
+        Log.d("Testing","Course offering clicked");
+
+
+    }
+
+
+
+}
